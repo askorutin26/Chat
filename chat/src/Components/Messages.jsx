e052add7239fe615428f37f0e4e40151aa8760f0
askor26 <askorutin26@gmail.com>
2022-08-08 05:14:44 +0300

    added scroll to the last message

--- chat/src/Components/Messages.jsx
+++ chat/src/Components/Messages.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useAppContext } from '../Hooks/index.js';
 
 const filter = require('leo-profanity');
 
 function Messages() {
   const { messages, activeChannelId } = useAppContext();
+  const refElem = useRef();
+  useEffect(() => {
+    if (refElem.current !== undefined) {
+      refElem.current.scrollIntoView();
+    }
+  }, [messages]);
 
   const channelMessages = messages.filter(
     (message) => message.channelId === activeChannelId,
@@ -12,8 +18,9 @@ function Messages() {
 
   return channelMessages.map((message) => {
     const { body, id, username } = message;
+
     return (
-      <div className="text-break mb-2" id={id} key={id}>
+      <div className="text-break mb-2" id={id} key={id} ref={refElem}>
         <b>
           { username }
         </b>
